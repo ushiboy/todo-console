3944b98dec7723e6831e025ceeca9a952dd2c986
ushiboy <ushiboy.dev@gmail.com>
2016-06-05 21:48:20 +0900

    working

--- src/scripts/backend.js
+++ src/scripts/backend.js
@@ -1,4 +1,31 @@
 import { createServer } from './lib';
+import { match } from './lib/router';
 import routes from './routes';
 
-createServer(routes).listen(3001);
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+
+
+function application(request) {
+  const { url, headers } = request;
+  const matched = match(url, routes);
+  if (matched) {
+    return [index(renderToString(<matched.handler />)), 200];
+  }
+  return ['Not Found', 404];
+}
+
+function index(mainView) {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Extreme Todo</title>
+</head>
+<body>
+<div id="app">${mainView}</div>
+</body>
+</html>`;
+}
+
+createServer(application).listen(3001);
--- src/scripts/lib/server.js
+++ src/scripts/lib/server.js
@@ -1,21 +1,12 @@
 import connect from 'connect';
 import http from 'http';
-import { match } from './router';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
 
-export function createServer(routes) {
+export function createServer(application) {
   const app = connect();
   app.use('/', (req, res) => {
-    const { url, headers } = req;
-    const matched = match(url, routes);
     res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-    if (matched) {
-      const html = renderToString(<matched.handler />);
-      res.end(html);
-    } else {
-      res.end('Not Found', 404);
-    }
+    const [html, statusCode] = application(req);
+    res.end(html, statusCode);
   });
   return http.createServer(app);
 }
